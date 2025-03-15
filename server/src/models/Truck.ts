efda913db3aaa74b49f426ba2149a4af8f684494
pechikonar <muthukonaruot@gmail.com>
2025-03-15 12:58:58 -0400

    Added couple of Columns like Truckname and Truck Capacity in Tryuck.ts Model

--- server/src/models/Truck.ts
+++ server/src/models/Truck.ts
@@ -3,6 +3,8 @@ import mongoose from 'mongoose';
 
 interface ITruck extends Document {
   truckId: string;
+  truckName: string;
+  truckCapacity: number;
   driverName: string;
   status: 'Available' | 'In Transit';
   assignedWarehouse: mongoose.Types.ObjectId;
@@ -14,6 +16,13 @@ const truckSchema = new Schema<ITruck>(
       type: String,
       required: true, 
       unique: true 
+    },
+    truckName: { 
+      type: String,
+      required: true, 
+    },
+    truckCapacity: { 
+      type: Number,
     },
     driverName: { 
       type: String, 
