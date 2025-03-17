5d5a42342799e1d235abb083ea9323c2264897ab
gilmerperez <gilmer2002@outlook.com>
2025-03-17 13:16:43 -0400

    Formatted resolvers.ts file, made no changes

--- server/src/schemas/resolvers.ts
+++ server/src/schemas/resolvers.ts
@@ -1,9 +1,9 @@
 import { User } from '../models/index.js';
-import { signToken, AuthenticationError } from '../utils/auth.js'; 
+import { signToken, AuthenticationError } from '../utils/auth.js';
 
 // Define types for the arguments
 interface AddUserArgs {
-  input:{
+  input: {
     username: string;
     email: string;
     password: string;
@@ -19,7 +19,6 @@ interface LoginUserArgs {
 interface UserArgs {
   username: string;
 }
-
 
 const resolvers = {
   Query: {
@@ -45,34 +44,34 @@ const resolvers = {
     addUser: async (_parent: any, { input }: AddUserArgs) => {
       // Create a new user with the provided username, email, and password
       const user = await User.create({ ...input });
-    
+
       // Sign a token with the user's information
       const token = signToken(user.username, user.email, user._id, user.role);
-    
+
       // Return the token and the user
       return { token, user };
     },
-    
+
     login: async (_parent: any, { email, password }: LoginUserArgs) => {
       // Find a user with the provided email
       const user = await User.findOne({ email });
-    
+
       // If no user is found, throw an AuthenticationError
       if (!user) {
         throw new AuthenticationError('Could not authenticate user.');
       }
-    
+
       // Check if the provided password is correct
       const correctPw = await user.isCorrectPassword(password);
-    
+
       // If the password is incorrect, throw an AuthenticationError
       if (!correctPw) {
         throw new AuthenticationError('Could not authenticate user.');
       }
-    
+
       // Sign a token with the user's information
       const token = signToken(user.username, user.email, user._id, user.role);
-    
+
       // Return the token and the user
       return { token, user };
     }
