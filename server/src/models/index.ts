9f3b29a2e2156264d67715dba1e720a25e71385f
aMononito <pedro@torstig.us>
2025-03-17 13:30:36 -0400

    -commit added logos and navbar updates

--- server/src/models/index.ts
+++ server/src/models/index.ts
@@ -2,9 +2,8 @@
 import User from './User.js';
 import Truck from './Truck.js';
 import Warehouse from './Warehouse.js';
-import Inventory from './Inventory.js';
 
 
 
 
-export {User, Truck, Warehouse, Inventory};
+export {User, Truck, Warehouse};
