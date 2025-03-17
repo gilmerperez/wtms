99e7a352876e8489666df3a4ab33dd06b1a8f2b4
gilmerperez <gilmer2002@outlook.com>
2025-03-17 13:15:41 -0400

    Formatted User.tsx

--- client/src/interfaces/User.tsx
+++ client/src/interfaces/User.tsx
@@ -1,9 +1,8 @@
 export interface User {
-    username: string | null;
-    password: string | null;
-    firstName: string | null;
-    lastName: string | null;
-    email: string | null;
-    role: string | null;
-  }
-  
+  username: string | null;
+  password: string | null;
+  firstName: string | null;
+  lastName: string | null;
+  email: string | null;
+  role: string | null;
+}
