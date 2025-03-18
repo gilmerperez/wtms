ccdbd434d030268dc92bbcdbbc939ff7285798e8
gilmerperez <gilmer2002@outlook.com>
2025-03-18 13:14:47 -0400

    Fixed css import

--- client/src/pages/UserMgt.tsx
+++ client/src/pages/UserMgt.tsx
@@ -1,4 +1,4 @@
-import "./UserMgt.css";
+import '../styles/UserMgt.css'
 import React, { useState, useEffect } from "react";
 import { useQuery, useMutation } from "@apollo/client";
 import { QUERY_USERS } from "../utils/queries";
