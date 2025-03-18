6a5f92b2522b881ee2a89b2e35ca55aa9874d230
rs0579 <raheemsenegal@outlook.com>
2025-03-18 16:50:00 +0100

    Added query to typeDefs, moved the me to the query from mutations, changed data types in resolvers, added updateWarehouseCapacity function.

--- server/src/schemas/resolvers.ts
+++ server/src/schemas/resolvers.ts
@@ -5,15 +5,15 @@ import { signToken, AuthenticationError } from '../utils/auth.js'
 
 // Define types for the arguments
 interface AddUserArgs {
-    username: string,
-    email: string,
-    password: string,
-    role: string,
-    status: boolean,
-    isCorrectPassword: boolean
+  username: string,
+  email: string,
+  password: string,
+  role: string,
+  status: boolean,
+  isCorrectPassword: boolean
 }
-interface UpdateUserStatus{
-  userId: number,
+interface UpdateUserStatus {
+  userId: string,
   status: boolean
 }
 
@@ -23,13 +23,13 @@ interface LoginUserArgs {
 }
 
 interface UserArgs {
-  userId: number
+  userId: string
 }
 interface WarehouseArgs {
-  warehouseId: number
+  warehouseId: string
 }
 interface TruckArgs {
-  truckId: number
+  truckId: string
 }
 
 interface addWarehouse {
@@ -67,9 +67,16 @@ const resolvers = {
     getTrucks: async () => {
       return await Truck.find()
     },
-
     getTruck: async (_parent: unknown, { truckId }: TruckArgs) => {
       return await Truck.findOne({ _id: truckId });
+    },
+    me: async (_parent: unknown, _args: unknown, context: any) => {
+      // If the user is authenticated, find and return the user's information along with their thoughts
+      if (context.user) {
+        return User.findOne({ _id: context.user._id });
+      }
+      // If the user is not authenticated, throw an AuthenticationError
+      throw new AuthenticationError('Could not authenticate user.');
     },
   },
   Mutation: {
@@ -88,47 +95,47 @@ const resolvers = {
       const token = signToken(user.email, user.password, user.role, user.status)
       return { token, user }
     },
-    addUser: async (_parent: unknown, {username, email, password, role, status, isCorrectPassword}: AddUserArgs) => {
-      const newUser = await User.create({ username, email, password, role, status, isCorrectPassword});
+    addUser: async (_parent: unknown, { username, email, password, role, status, isCorrectPassword }: AddUserArgs) => {
+      const newUser = await User.create({ username, email, password, role, status, isCorrectPassword });
       // User.save()
       return newUser;
     },
     addWarehouse: async (_parent: unknown, { name, location, status, items, quantity }: addWarehouse) => {
-      const newWarehouse = Warehouse.create({name, location, status, items, quantity});
+      const newWarehouse = Warehouse.create({ name, location, status, items, quantity });
       return newWarehouse;
     },
     addTruck: async (_parent: unknown, { truckId, truckName, truckCapacity, driverName, status, assignedWarehouse }: AddTruck) => {
-      const newTruck = Truck.create({truckId, truckName, truckCapacity, driverName, status, assignedWarehouse});
+      const newTruck = Truck.create({ truckId, truckName, truckCapacity, driverName, status, assignedWarehouse });
 
       return newTruck;
     },
-    addItem: async (_parent:unknown, {warehouseId, item}: {warehouseId: number, item: string}) => {
+    addItem: async (_parent: unknown, { warehouseId, item }: { warehouseId: string, item: string }) => {
       return await Warehouse.findByIdAndUpdate(
         warehouseId,
-         {$push: {items: item}},
-        {new: true, runValidators: true}
+        { $push: { items: item } },
+        { new: true, runValidators: true }
       );
     },
-    updateItem: async (_parent: unknown, {warehouseId, index, newItem}: {warehouseId: number, index: number, newItem: string}) => {
+    updateItem: async (_parent: unknown, { warehouseId, index, newItem }: { warehouseId: string , index: number, newItem: string }) => {
       return await Warehouse.findOneAndUpdate(
-        {_id: warehouseId},
+        { _id: warehouseId },
         //CHECK TO SEE IF THIS IS CORRECT SYNTAX
-        {$set: {[`items.${index}`]: newItem}},
-        {new: true, runValidators: true}
+        { $set: { [`items.${index}`]: newItem } },
+        { new: true, runValidators: true }
       )
     },
-    deleteItem: async(_parent: unknown, {warehouseId, item}: {warehouseId: number, item: string}) =>{
+    deleteItem: async (_parent: unknown, { warehouseId, item }: { warehouseId: string, item: string }) => {
       return await Warehouse.findByIdAndUpdate(
         warehouseId,
-        {$pull: {items:item}},
-        {new:true, runValidators: true}
+        { $pull: { items: item } },
+        { new: true, runValidators: true }
       )
     },
     updateUserStatus: async (_parent: unknown, { userId, status }: UpdateUserStatus) => {
       return await User.findByIdAndUpdate(
         { _id: userId },
         // {$addToSet: {status: Boolean}},
-        { $addToSet: {status: status }},
+        { $addToSet: { status: status } },
 
         { new: true, runValidators: true }
       )
@@ -138,19 +145,20 @@ const resolvers = {
     //     return await User.findByIdAndDelete(userId);
     // },
     //WE NEED TO DELETE SOMETHING TO FULFILL THE PROJECT'S CRITERIA
-    deleteTruck: async (_parent: unknown, {truckId}: TruckArgs) => {
+    deleteTruck: async (_parent: unknown, { truckId }: TruckArgs) => {
       return await Truck.findByIdAndDelete(truckId)
     },
-    deleteWarehouse: async (_parent: unknown, {warehouseId}: WarehouseArgs) => {
+    deleteWarehouse: async (_parent: unknown, { warehouseId }: WarehouseArgs) => {
       return await Warehouse.findByIdAndDelete(warehouseId)
     },
-    // updateWarehouseStatus: async (_parent: unknown, { warehouseId, status }) => {
-    //   return await Warehouse.findByIdAndUpdate(
-    //     { _id: warehouseId },
-    //     { $addToSet: {status: status} },
-    //     { new: true, runValidators: true }
-    //   );
-    // },
+    updateWarehouseCapacity: async (_parent: unknown, { warehouseId, capacity }: { warehouseId: string, capacity: number }) => {
+      return await Warehouse.findByIdAndUpdate(
+        { _id: warehouseId },
+        //     { $addToSet: {status: status} },
+        { $set: { capacity: capacity } },
+        { new: true, runValidators: true }
+      );
+    },
     // updateTruckStatus: async (_parent: unknown, { truckId, status }) => {
     //   return await Truck.findByIdAndUpdate(\
     //     { _id: truckId },
@@ -161,14 +169,7 @@ const resolvers = {
 
     // Query to get the authenticated user's information
     // The 'me' query relies on the context to check if the user is authenticated
-    me: async (_parent: unknown, _args: unknown, context: any) => {
-      // If the user is authenticated, find and return the user's information along with their thoughts
-      if (context.user) {
-        return User.findOne({ _id: context.user._id });
-      }
-      // If the user is not authenticated, throw an AuthenticationError
-      throw new AuthenticationError('Could not authenticate user.');
-    },
+
   }
 }
 export default resolvers
