f8e615632a5f3d8c8d4715caf67baa7c201a7fc2
gilmerperez <gilmer2002@outlook.com>
2025-03-20 17:07:27 -0400

    Styled the page and formatted it as well

--- client/src/pages/Fleet.tsx
+++ client/src/pages/Fleet.tsx
@@ -1,18 +1,17 @@
-import { useQuery, useMutation } from "@apollo/client";
-import { QUERY_ALL_TRUCKS } from "../utils/queries";
-import { REMOVE_TRUCK } from "../utils/mutations";
 import auth from "../utils/auth";
+import { REMOVE_TRUCK } from "../utils/mutations";
+import { QUERY_ALL_TRUCKS } from "../utils/queries";
+import { useQuery, useMutation } from "@apollo/client";
+
 const Fleet = () => {
+  const { loading, data } = useQuery(QUERY_ALL_TRUCKS);
+  const userData = data?.getTrucks || [];
 
-  const { loading, data } = useQuery(QUERY_ALL_TRUCKS)
-  //VERIFY IF THE .TRUCKS PART IS TOTALLY NECESSARY
-  const userData = data?.getTrucks || []
-
-  const [removeTruck] = useMutation(REMOVE_TRUCK)
+  const [removeTruck] = useMutation(REMOVE_TRUCK);
 
   const handleDeleteTruck = async (e: any, truckId: string) => {
-    e.preventDefault()
-    console.log(truckId)
+    e.preventDefault();
+    console.log(truckId);
     const token = auth.loggedIn() ? auth.getToken() : null;
     if (!token) {
       return false;
@@ -20,59 +19,64 @@ const Fleet = () => {
     try {
       const response = await removeTruck({
         variables: {
-          truckId
-        }
+          truckId,
+        },
       });
 
       if (!response.data || !response.data.removeTruck) {
-        throw new Error('Something went wrong, please try again.')
+        throw new Error("Something went wrong, please try again.");
       }
-      // removeTruck(userData.truckId)
-    }
-    catch (err) {
-      console.error(err)
+    } catch (err) {
+      console.error(err);
     }
   };
 
   if (loading) {
-    return <h2>Loading...</h2>
+    return <h2>Loading...</h2>;
   }
 
-  //I NEED TO ACCESS THE SEED DATA/DATABASE TO BE ABLE TO EXTRACT THAT DATA AND APPLY IT TO MY TABLE
-  //
-  // const tableRow =
-  console.log(userData)
   return (
-    <main>
-      <h1>Fleet Management</h1>
-      <table>
-        <tr>
-          <th>Truck ID</th>
-          <th>Truck Name</th>
-          <th>Truck Capacity</th>
-          <th>Driver</th>
-          <th>Truck Status</th>
-          <th>Truck Warehouse</th>
-          <th>Remove</th>
-        </tr>
-        {userData.map((user: any) => {
-          return (
-            <tr key={user._id}>
-              <td>{user.truckId}</td>
-              <td>{user.truckName}</td>
-              <td>{user.truckCapacity}</td>
-              <td>{user.driverName}</td>
-              <td>{user.status}</td>
-              <td>{user.assignedWarehouse.name}</td>
-              <button
-            onClick={(e) => handleDeleteTruck(e, user.truckId)}
-          >
-            Remove Truck
-          </button>
-            </tr>
-          )
-        })}
-      </table>
+    <main className="container-fluid px-3">
+      <h1 className="heading text-center my-4">Fleet Management</h1>
+
+      {/* Fleet Table Section */}
+      <div className="section">
+        <div className="table-responsive">
+          <table className="table table-striped">
+            <thead>
+              <tr>
+                <th>Truck ID</th>
+                <th>Truck Name</th>
+                <th>Truck Capacity</th>
+                <th>Driver</th>
+                <th>Truck Status</th>
+                <th>Truck Warehouse</th>
+                <th>Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              {userData.map((user: any) => (
+                <tr key={user._id}>
+                  <td>{user.truckId}</td>
+                  <td>{user.truckName}</td>
+                  <td>{user.truckCapacity}</td>
+                  <td>{user.driverName}</td>
+                  <td>{user.status}</td>
+                  <td>{user.assignedWarehouse.name}</td>
+                  <td>
+                    <button
+                      onClick={(e) => handleDeleteTruck(e, user.truckId)}
+                      className="btn btn-danger btn-sm"
+                    >
+                      Remove Truck
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
     </main>
   );
 };
