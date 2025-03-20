f368d0a20a406397c1d839cea461fc50409f53ac
pechikonar <muthukonaruot@gmail.com>
2025-03-20 00:06:43 -0400

    Changed the logo appropriate to dark theme

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,5 +1,5 @@
 // import React from "react";
-import logo from "../assets/logo_noBg.png";
+import logo from "../assets/logo_darkTheme.png";
 
 
 const Header = () => {
@@ -8,7 +8,7 @@ const Header = () => {
       <nav className="navbar navbar-expand-lg navbar-dark">
       {/* <div className="container d-flex justify-content-between align-items-center">
       Left Image */}
-          <img src={logo} alt="WTSM" className="header-img" style={{ height: "80px" }} />
+          <img src={logo} alt="WTSM" className="header-img" style={{ height: "90px" }} />
         
           <h1 className="m-1">WareHouse and Transport Management System</h1>
           
